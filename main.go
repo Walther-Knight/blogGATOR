75ceb84669a2f1829dd4db2172154d98e4106870
walther_knight <knightofargh@gmail.com>
2025-05-13 16:52:46 -0400

    Implemented command functionality

--- main.go
+++ main.go
@@ -7,27 +7,69 @@ import (
 	"github.com/Walther-Knight/blogGATOR/internal/config"
 )
 
+type state struct {
+	*config.Config
+}
+
+type command struct {
+	name string
+	args []string
+}
+
+type commands struct {
+	cmds map[string]func(*state, command) error
+}
+
+func handlerLogin(s *state, cmd command) error {
+	if len(cmd.args) == 0 {
+		return fmt.Errorf("invalid command: username required")
+	}
+	err := s.SetUser(cmd.args[0])
+	if err != nil {
+		return fmt.Errorf("set user failed: %w", err)
+	}
+	fmt.Println("User value set in config")
+	return nil
+}
+
+func (c *commands) run(s *state, cmd command) error {
+	handler, exists := c.cmds[cmd.name]
+	if !exists {
+		return fmt.Errorf("unknown command: %s", cmd.name)
+	}
+	return handler(s, cmd)
+}
+
+func (c *commands) register(name string, f func(*state, command) error) {
+	c.cmds[name] = f
+}
+
 func main() {
-	// Read initial config
 	cfg, err := config.Read()
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Error reading config: %v\n", err)
 		os.Exit(1)
 	}
+	appState := &state{Config: &cfg}
+	cmds := commands{
+		make(map[string]func(*state, command) error),
+	}
+	cmds.register("login", handlerLogin)
 
-	// Set user
-	if err := cfg.SetUser("brent"); err != nil {
-		fmt.Fprintf(os.Stderr, "Error setting user: %v\n", err)
+	args := os.Args
+	if len(args) < 2 {
+		fmt.Fprintf(os.Stderr, "not enough arguments\n")
 		os.Exit(1)
 	}
 
-	// Read config again to verify changes
-	updatedCfg, err := config.Read()
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "Error reading updated config: %v\n", err)
+	cmd := command{
+		name: args[1],
+		args: args[2:],
+	}
+
+	if err := cmds.run(appState, cmd); err != nil {
+		fmt.Fprintf(os.Stderr, "Error: %v\n", err)
 		os.Exit(1)
 	}
 
-	// Print the config
-	fmt.Printf("%+v\n", updatedCfg)
 }
